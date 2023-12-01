f9d761b551f5201f758f0f75c64ff370e6e45a53
Gopal Sangubhotla <saibhotla@gmail.com>
2023-12-01 01:31:26 -0600

    python cors and session cookie changes

--- test.py
+++ test.py
@@ -1,16 +1,24 @@
 
-from fastapi import FastAPI
+from fastapi import Cookie, Depends, FastAPI, HTTPException, Request, Response
+from fastapi import security
 from fastapi.middleware.cors import CORSMiddleware
 import csv
 from fastapi.responses import JSONResponse
 import json
+from starlette.status import HTTP_401_UNAUTHORIZED
 
+from fastapi.security import HTTPBasic, HTTPBasicCredentials, OAuth2PasswordRequestForm
+security = HTTPBasic()
 app = FastAPI()
-
+origins = [
+    "http://localhost:3000",  # React app's origin
+    # Add any other origins that need to access your API
+]
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -39,6 +47,53 @@ def get_data():
 def hello():
     return 'Hello, World!'
 
+# @app.post('/login')
+# def login(request: Request, response: Response):
+#     # Perform login logic here
+#     # Set the session cookie
+#     response.set_cookie(key="session", value="your_session_value", httponly=True)
+#     return {"message": "Login successful"}
+
+
+def get_current_user(session: str = Cookie(None)):
+    # This is a placeholder implementation. In a real application, you would
+    # use the session cookie to look up the user in your database.
+    if session == 'your_session_value':
+        return {'username': 't@gmail.com'}
+    else:
+        raise HTTPException(
+            status_code=HTTP_401_UNAUTHORIZED,
+            detail="Not authenticated",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+@app.get('/current_user')
+def current_user(user = Depends(get_current_user)):
+    return user
+
+@app.post('/logout')
+def logout(response: Response):
+    # Clear the session cookie
+    response.delete_cookie(key="session")
+    return {"message": "Logout successful"}
+
+
+@app.post('/login')
+def login(response: Response, form_data: OAuth2PasswordRequestForm = Depends()):
+    # This is a placeholder implementation. In a real application, you would
+    # verify the user's credentials against your database.
+    if form_data.username == 't@gmail.com' and form_data.password == 'test':
+        # Set the session cookie
+        response.set_cookie(key="session", value="your_session_value", httponly=True)
+        return {"message": "Login successful"}
+    else:
+        raise HTTPException(
+            status_code=HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app, host='0.0.0.0', port=8000)
+
